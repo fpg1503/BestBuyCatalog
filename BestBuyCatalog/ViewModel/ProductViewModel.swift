552e433a4400a360b96527cbe5ff431a7e3c71a3
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-10-06 19:49:31 -0300

    Add ProductViewModel conformance to Product

--- BestBuyCatalog/ViewModel/ProductViewModel.swift
+++ BestBuyCatalog/ViewModel/ProductViewModel.swift
@@ -13,3 +13,29 @@ protocol ProductViewModel {
 
     var accessibilityLabel: String { get }
 }
+
+extension Product: ProductViewModel {
+    var formattedDiscount: String {
+        let discount = regularPrice - salePrice
+        let discountPercentage = (discount/regularPrice) * 100
+
+        return String(format: "%2f%%", discountPercentage)
+    }
+
+    var formattedPrice: String {
+        let formatter = NumberFormatter.currencyFormatter()
+        let formattedRegularPrice = formatter.string(from: NSNumber(value: regularPrice)) ?? ""
+        let formattedSalePrice = formatter.string(from: NSNumber(value: salePrice)) ?? ""
+
+        if onSale && salePrice < regularPrice {
+            return "De " + formattedRegularPrice + " por: " + formattedSalePrice
+        } else {
+            return formattedSalePrice
+        }
+    }
+
+    var accessibilityLabel: String {
+        let sale = onSale ? " Em promoção: " : " "
+        return name + sale + formattedPrice
+    }
+}
