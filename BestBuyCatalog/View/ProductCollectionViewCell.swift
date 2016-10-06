636b1e33b275933b45a3e9ad50cbc1a31e2da331
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-10-06 19:55:14 -0300

    Add ProductViewModelConfigurable conformance to ProductCollectionViewCell

--- BestBuyCatalog/View/ProductCollectionViewCell.swift
+++ BestBuyCatalog/View/ProductCollectionViewCell.swift
@@ -1,4 +1,5 @@
 import UIKit
+import AlamofireImage
 
 public final class ProductCollectionViewCell: UICollectionViewCell {
     @IBOutlet private var productImageView: UIImageView?
@@ -6,4 +7,21 @@ public final class ProductCollectionViewCell: UICollectionViewCell {
     @IBOutlet private var discountLabel: UILabel?
     @IBOutlet private var productNameLabel: UILabel?
     @IBOutlet private var productPriceLabel: UILabel?
+
+    func configure(with productViewModel: ProductViewModel) {
+        if let image = productViewModel.thumbnailImageURL {
+            productImageView?.af_setImage(withURL: image)
+        } else {
+            logFailedPrecondition("Product has no image")
+        }
+
+        discountLabelContainer?.isHidden = !productViewModel.onSale
+        discountLabel?.text = productViewModel.formattedDiscount
+        productNameLabel?.text = productViewModel.name
+        productPriceLabel?.text = productViewModel.formattedPrice
+
+        //TODO: Accessibility!
+    }
 }
+
+extension ProductViewModelConfigurable { }
