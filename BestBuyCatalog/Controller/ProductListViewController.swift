ebd532c70e016c30df028edbf3099cc7b0f927e4
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-10-06 21:05:08 -0300

    Add UICollectionViewDataSource conformance to ProductListViewController

--- BestBuyCatalog/Controller/ProductListViewController.swift
+++ BestBuyCatalog/Controller/ProductListViewController.swift
@@ -1,5 +1,22 @@
 import UIKit
 
 public final class ProductListViewController: UIViewController {
+    @IBOutlet var collectionView: UICollectionView?
 
+    var products: [Product] = []
+}
+
+extension ProductListViewController: UICollectionViewDataSource {
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        //TODO
+        return UICollectionViewCell()
+    }
 }
