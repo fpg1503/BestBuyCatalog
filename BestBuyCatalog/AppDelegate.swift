b6cecf9ba5e5799cd58b4af150809dac4ab1790c
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-10-09 17:50:46 -0300

    Use black tint for navigation bar

--- BestBuyCatalog/AppDelegate.swift
+++ BestBuyCatalog/AppDelegate.swift
@@ -3,4 +3,10 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        UINavigationBar.appearance().tintColor = .black
+        return true
+    }
+    
 }
