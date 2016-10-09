d43613ace34da383666b114c0ffcece52c4137b2
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2016-10-09 17:18:04 -0300

    Add UIViewController+ImageController

--- BestBuyCatalog/Controller/ProductDetailsViewController+ImageZoom.swift
+++ BestBuyCatalog/Controller/ProductDetailsViewController+ImageZoom.swift
@@ -1,17 +1,9 @@
 import UIKit
-import JTSImageViewController
 
 extension ProductDetailsViewController {
     @IBAction func showImage() {
         guard let imageView = imageView else { return }
 
-        let imageInfo = JTSImageInfo()
-        imageInfo.image = imageView.image
-        imageInfo.referenceRect = imageView.frame
-        imageInfo.referenceView = imageView
-
-        let imageViewer = JTSImageViewController(imageInfo: imageInfo, mode: .image, backgroundStyle: .scaled)
-
-        imageViewer?.show(from: self, transition: .fromOriginalPosition)
+        show(image: imageView.image, from: imageView)
     }
 }
